40a9c5a75b73d5c9bb71849f5f0d05a4a8f11dd1
obswork <obssabizuwork@gmail.com>
2016-05-30 12:23:05 -0400

    added a features inline model on the home page as well as some experimental WIP

--- home/models.py
+++ home/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, \
     InlinePanel, StreamFieldPanel
-from wagtail.wagtailcore.fields import BooleanField
+from wagtail.wagtailcore.fields import BooleanField, RichTextField
 
 
 from modelcluster.fields import ParentalKey
 
-from utils.models import RelatedLink
+# from utils.models import RelatedLink
 from articles.models import ArticlePage
 
 
+"""
+# home page related links needed?
 class HomePageRelatedLink(Orderable, RelatedLink):
     page = ParentalKey('home.HomePage', related_name='related_links')
+"""
 
 
 class HomePagePost(Orderable):
@@ -25,15 +28,33 @@ class HomePagePost(Orderable):
     page = ParentalKey('home.HomePage', related_name='home_posts')
 
 
+class Feature(models.Model, Orderable)
+    body = RichTextField()
+
+    page = ParentalKey('home.HomePage', related_name='features')
+
+    panels = [
+        FieldPanel('body'),
+    ]
+
+    """
+    # TODO: make feature an abstract model?
+    class Meta:
+        abstract = True
+    """
+
+
 class HomePage(Page):
-    auto = BooleanField(default=True, label="Auto Homepage")
+    auto_post = BooleanField(default=True, label="Auto frontpage posts")
+    # auto_sidebar not viable..for now..probably
+    # auto_sidebar = BooleanField(default=True, label="Auto sidebar posts")
 
     @property
     def posts(self):
         # number of posts to display on homepage
         count = 3
-        # if auto is set on the homepage, front page posts will by 3 most recent articles from db
-        if self.auto:
+        # if auto_post is set on the homepage, front page posts will be 3 most recent articles from db
+        if self.auto_post:
             posts = ArticlePage.objects.live().order_by('-date')[:count]
             return posts
         # else front page posts will be those selected in the admin (inline panel) (+ enough recent articles to keep home page reasonably populated - default = 3)
@@ -49,13 +70,26 @@ class HomePage(Page):
             else:
                 return posts
 
+    @property
+    def mini_posts(self):
+        """
+        # number of sidebar miniposts to display
+        count = 4
+        # see above note about auto_sidebar 
+        # if auto_sidebar is set on the homepage, front side bar posts will be a random selection of site content + embeds
+        if self.auto_sidebar:
+            posts =  
+        """
+
+
     class Meta:
         verbose_name = "Homepage"
 
 HomePage.content_panels = [
     FieldPanel('title', classname="full title"),
-    FieldPanel('auto', label="Auto update home page?"),
+    FieldPanel('auto_post', label="Auto update home page?"),
     StreamFieldPanel('body'),
-    InlinePanel('related_links', label="Related links"),
-    InlinePanel('home_posts', label="homepage posts")
+    # InlinePanel('related_links', label="Related links"),
+    InlinePanel('features', label="Homepage Featured Content"),
+    InlinePanel('home_posts', label="Homepage Posts")
 ]
